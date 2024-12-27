715f8b90912b8a2e8928683bb18cd490afd81434
Bishwonath <suvamaryal3@gmail.com>
2024-12-27 14:57:01 +0545

    Cubit view

---
+++ lib/cubit/Calculator.cubit.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CalculatorCubit extends Cubit<double> {
+  CalculatorCubit() : super(0);
+
+  void add(double num1, double num2) {
+    emit(num1 + num2);
+  }
+
+  void subtract(double num1, double num2) {
+    emit(num1 - num2);
+  }
+
+  void multiply(double num1, double num2) {
+    emit(num1 * num2);
+  }
+
+  void divide(double num1, double num2) {
+    emit(num1 / num2);
+  }
+
+  void reset() {
+    emit(0);
+  }
+}
